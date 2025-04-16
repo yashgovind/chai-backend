bf572f8f0cc1f2393f1b666db6266c0c9444de60
Yash <yashgovind123@gmail.com>
2025-04-17 01:42:00 +0530

    Implement channel stats retrieval in dashboard controller

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -5,10 +5,95 @@ import {Like} from "../models/like.model.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
+import { group } from "console"
 
 const getChannelStats = asyncHandler(async (req, res) => {
     // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
-})
+    const userId = req.user?._id;
+    // get total videos.
+    const totalVideos = await Video.aggregate([
+     {$match:{owner:userId}},
+       {
+           $group: {
+               _id: null,
+               count:{$sum:1}
+          }
+       },
+       {$sort:{count:-1}}
+   ])
+
+    // get total subscribers.
+    const totalSubscribers = await Subscription.aggregate([
+        {$match:{owner:userId}},
+        {
+            $group: {
+                _id: "$channel",
+                count:{$sum:1}
+            }
+        },
+        {$sort:{count:-1}}
+    ])
+
+
+    // get total likes
+    const totalLikes = await Like.aggregate([
+        {$match:{LikedBy:userId}},
+        {
+            $group: {
+                _id: null,
+                count:{$sum:1}
+            }
+        },
+        {$sort:{count:-1}}
+    ])
+
+    // get total Views.
+    const totalViews = await Video.aggregate([
+        {$match:{owner:userId}},
+        {
+            $group: {
+                _id: "$views",
+                count:{$sum:1}
+            }
+        },
+        {$sort:{count:-1}}
+    ])
+
+    const totalTweets = await Tweet.aggregate([
+        {$match:{owner:userId}},
+        {
+            $group: {
+                _id: null,
+                count:{$sum:1}
+            }
+        },
+        {$sort:{count:-1}}
+    ])
+
+    const totalComments = await Comment.aggregate([
+        {$match:{owner:userId}},
+        {
+            $group: {
+                _id: null,
+                count:{$sum:1}
+            }
+        },
+        {$sort:{count:-1}}
+    ])
+
+
+
+    return res.status(200).json(
+        new ApiResponse(200, {
+            totalVideos,
+            totalSubscribers,
+            totalLikes,
+            totalViews,
+            totalComments,
+            totalTweets
+        }, "Channel stats fetched successfully")
+    );
+});
 
 const getChannelVideos = asyncHandler(async (req, res) => {
     // TODO: Get all the videos uploaded by the channel
